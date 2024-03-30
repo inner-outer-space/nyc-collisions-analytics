56dffa40191a80db63d89178a9f1f0d5bd9d3396
inner-outer-space <lindasteinberger@gmail.com>
2024-03-30 21:06:18 +0100

    terraform

--- terraform-basic/main.tf
+++ terraform-basic/main.tf
@@ -13,13 +13,13 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_storage_bucket" "taxi-bucket" {
+resource "google_storage_bucket" "nyc-collisions-bucket" {
   name          = var.gcs_bucket_name
   location      = var.gcp_storage_location
   force_destroy = true
 }
 
-resource "google_bigquery_dataset" "taxi-dataset" {
+resource "google_bigquery_dataset" "nyc-collisions-dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.gcp_storage_location
 }
