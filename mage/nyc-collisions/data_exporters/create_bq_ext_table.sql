d3f2a88d930d2c3012b8a26b4dff404b87225542
inner-outer-space <lindasteinberger@gmail.com>
2024-04-18 21:45:01 +0200

    remove extra pipelines

--- mage/nyc-collisions/data_exporters/create_bq_ext_table.sql
+++
@@ -1,6 +0,0 @@
--- Docs: https://docs.mage.ai/guides/sql-blocks
-CREATE OR REPLACE EXTERNAL TABLE `nyc-auto-accidents.nyc_collisions.external_data`
-OPTIONS (
-    format = 'parquet',
-    uris = ['gs://collisions-first-try/ny_collision_data/*.parquet']
-);
