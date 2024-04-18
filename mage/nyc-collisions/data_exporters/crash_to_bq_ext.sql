29b7199f1dc75c9e58145dd6e6b0645f8a98a8aa
inner-outer-space <lindasteinberger@gmail.com>
2024-04-18 20:01:46 +0200

    clean up mage pipelines

--- mage/nyc-collisions/data_exporters/crash_to_bq_ext.sql
+++ mage/nyc-collisions/data_exporters/crash_to_bq_ext.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE EXTERNAL TABLE `nyc-auto-accidents.nyc_collisions.crash_external_data`
+CREATE OR REPLACE EXTERNAL TABLE `nyc-auto-accidents.nyc_collisions.crash_data_external`
 OPTIONS (
     format = 'parquet',
-    uris = ['gs://collisions-first-try/spark_transformed_data/*.parquet']
+    uris = ['gs://collisions-first-try/crash_data_spark_trans/*.parquet']
 );
 
