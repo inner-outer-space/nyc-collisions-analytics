f33e4dd4549b07869416f7dfedb9551d1a4d348f
inner-outer-space <lindasteinberger@gmail.com>
2024-04-18 21:45:01 +0200

    remove extra pipelines

--- mage/nyc-collisions/data_exporters/spark_gcs_to_bq_ext.sql
+++
@@ -1,7 +0,0 @@
--- Docs: https://docs.mage.ai/guides/sql-blocks
-CREATE OR REPLACE EXTERNAL TABLE `nyc-auto-accidents.nyc_collisions.spark_crash_external_data`
-OPTIONS (
-    format = 'parquet',
-    uris = ['gs://collisions-first-try/spark_transformed_data/*.parquet']
-);-- Docs: https://docs.mage.ai/guides/sql-blocks
-
