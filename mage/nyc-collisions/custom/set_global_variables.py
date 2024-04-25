1b3cc05a20177f10ec4ac911e70febecd9a78958
inner-outer-space <lindasteinberger@gmail.com>
2024-04-25 16:57:48 +0200

    mage pipeline small edits

--- mage/nyc-collisions/custom/set_global_variables.py
+++ mage/nyc-collisions/custom/set_global_variables.py
@@ -11,7 +11,7 @@ if 'test' not in globals():
 def set_global_variables(*args, **kwargs):
     """
     Set global variables programatically using env variables.
-    This makes it easier for someone to reproduce with their own gcs setup. 
+    Updates to the GCS project, bucket, etc can be made via the .env file. 
     """
 
     set_global_variable(kwargs['pipeline_uuid'], 'key_path', os.environ['GOOGLE_SERVICE_ACC_KEY_FILEPATH'])
