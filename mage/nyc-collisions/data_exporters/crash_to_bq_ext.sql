adb6e67ef016264e95f7ce11be325cbb161c2ac2
inner-outer-space <lindasteinberger@gmail.com>
2024-04-26 23:12:15 +0200

    adding single batch process pipeline

--- mage/nyc-collisions/data_exporters/crash_to_bq_ext.sql
+++ mage/nyc-collisions/data_exporters/crash_to_bq_ext.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE EXTERNAL TABLE `{{ google_project_id }}.{{ google_bq_dataset }}.crash_data_external`
 OPTIONS (
     format = 'parquet',
-    uris = ['gs://{{ google_bucket }}/{{ google_gcs_spark }}/collisions_batch_0.parquet']
+    uris = ['gs://{{ google_bucket }}/{{ google_gcs_spark }}/{{ output_file_name }}']
     --uris = ['gs://{{ google_bucket }}/{{ google_gcs_spark }}/*.parquet']
 );
 
