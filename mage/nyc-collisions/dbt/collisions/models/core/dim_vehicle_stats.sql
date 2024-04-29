012ed42a8c9377db68756924579ae1f60b3767c6
Linda Steinberger <lindasteinberger@gmail.com>
2024-04-29 08:00:52 +0000

    vehicle dim

--- mage/nyc-collisions/dbt/collisions/models/core/dim_vehicle_stats.sql
+++ mage/nyc-collisions/dbt/collisions/models/core/dim_vehicle_stats.sql
@@ -1,7 +1,15 @@
 WITH vehicle_join AS (
     SELECT
         vehicle,
-        contributing_factor
+        contributing_factor, 
+        total_persons_injured, 
+        total_persons_killed,
+        number_of_cyclist_injured,
+        number_of_cyclist_killed,
+        number_of_motorist_injured,
+        number_of_motorist_killed,
+        number_of_pedestrians_injured,
+        number_of_pedestrians_killed 
     FROM
         {{ ref('fact_crash_weather_enriched') }},
         UNNEST([vehicle_type_code_1, vehicle_type_code_2, vehicle_type_code_3, vehicle_type_code_4, vehicle_type_code_5]) AS vehicle
@@ -15,6 +23,14 @@ SELECT
     vehicle, 
     contributing_factor,
     COUNT(*) AS collision_count,
+    SUM(total_persons_injured) as persons_inj_count,
+    SUM(total_persons_killed) as persons_killed_count, 
+    SUM(number_of_cyclist_injured) as cycl_inj_count,
+    SUM(number_of_cyclist_killed) as cycl_killed_count, 
+    SUM(number_of_motorist_injured) as mot_inj_count, 
+    SUM(number_of_motorist_killed) as mot_killed_count, 
+    SUM(number_of_pedestrians_injured) as ped_inj_count, 
+    SUM(number_of_pedestrians_killed) as ped_killed_count
 FROM 
     vehicle_join
 GROUP BY 
