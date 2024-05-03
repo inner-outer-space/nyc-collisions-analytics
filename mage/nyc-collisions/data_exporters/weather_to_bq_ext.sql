b3ad73f76b817d9bec16aba692b6679f7351bdd8
inner-outer-space <lindasteinberger@gmail.com>
2024-05-03 13:46:40 +0200

    pipeline work

--- mage/nyc-collisions/data_exporters/weather_to_bq_ext.sql
+++ mage/nyc-collisions/data_exporters/weather_to_bq_ext.sql
@@ -1,5 +1,5 @@
 -- Docs: https://docs.mage.ai/guides/sql-blocks
-CREATE OR REPLACE EXTERNAL TABLE `{{ google_project_id }}.{{ google_bq_dataset }}.weather_data_external`
+CREATE OR REPLACE EXTERNAL TABLE `{{ google_project_id }}.{{ google_bq_dataset }}.external_weather_data`
 OPTIONS (
     format = 'parquet',
     uris = ['gs://{{ google_bucket }}/weather_data.parquet']
