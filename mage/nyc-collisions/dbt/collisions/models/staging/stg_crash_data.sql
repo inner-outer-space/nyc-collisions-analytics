c4c8c9990f22f11fe4aa7cb7569ee6f02c8a45b5
Linda Steinberger <lindasteinberger@gmail.com>
2024-05-03 13:42:05 +0000

    cleaned up stg crash data

--- mage/nyc-collisions/dbt/collisions/models/staging/stg_crash_data.sql
+++ mage/nyc-collisions/dbt/collisions/models/staging/stg_crash_data.sql
@@ -1,5 +1,4 @@
 {{ config(materialized='view') }}
-
 WITH formatted_data AS (
   SELECT
         -- UNIQUE KEY
@@ -19,14 +18,15 @@ WITH formatted_data AS (
         cross_street_name,
 
         -- PEOPLE DETAILS
-        --cast(number_of_persons_injured as integer) as number_of_persons_injured,
-        --cast(number_of_persons_killed as integer) as number_of_persons_killed,
-        cast(number_of_pedestrians_injured as integer) as number_of_pedestrians_injured,
-        cast(number_of_pedestrians_killed as integer) as number_of_pedestrians_killed,
-        cast(number_of_cyclist_injured as integer) as number_of_cyclist_injured,
-        cast(number_of_cyclist_killed as integer) as number_of_cyclist_killed,
-        cast(number_of_motorist_injured as integer) as number_of_motorist_injured,
-        cast(number_of_motorist_killed as integer) as number_of_motorist_killed,
+        CAST(number_of_persons_injured as integer) AS number_of_persons_injured,
+        CAST(number_of_persons_killed as integer) AS number_of_persons_killed,
+        number_of_pedestrians_injured,
+        number_of_pedestrians_killed, 
+        number_of_cyclist_injured,
+        number_of_cyclist_killed,
+        number_of_motorist_injured,
+        number_of_motorist_killed,
+    
         
         -- REASON DETAILS 
         contributing_factor_vehicle_1,
@@ -51,51 +51,12 @@ SELECT
     crash_id,
 
     -- TIME_FIELDS (local timezone - NYC)
-    crash_timestamp_string, 
     parse_datetime('%Y-%m-%d %H:%M', crash_timestamp_string) as crash_datetime,
-    extract(date from crash_datetime) as crash_date,
-    extract(time from crash_datetime) as crash_time,
-    extract(month from crash_datetime) as crash_month, 
-    extract(year from crash_datetime) as crash_year, 
-    extract(dayofweek from crash_datetime) as crash_dow,
-    format_timestamp('%A', crash_datetime) as crash_dow_name,
-    format_timestamp('%Y-%m', crash_datetime) as crash_yr_mon,
-    sun_phase, 
 
 
-    -- LOCATION FIELDS 
-    borough,
-    zip_code,
-    latitude,
-    longitude,
-    on_street_name,
-    off_street_name,
-    cross_street_name,
-
-    -- PEOPLE DETAILS
-    --number_of_persons_injured,
-    --number_of_persons_killed as integer,
-    number_of_pedestrians_injured,
-    number_of_pedestrians_killed, 
-    number_of_cyclist_injured,
-    number_of_cyclist_killed,
-    number_of_motorist_injured,
-    number_of_motorist_killed,
+    -- THE REST OF THE COLUMNS
+    formatted_data.* EXCEPT(crash_id, crash_timestamp_string )
     
-    -- REASON DETAILS 
-    contributing_factor_vehicle_1,
-    contributing_factor_vehicle_2,
-    contributing_factor_vehicle_3,
-    contributing_factor_vehicle_4,
-    contributing_factor_vehicle_5,
-
-    -- CARS INVOLVED
-    vehicle_type_code_1,
-    vehicle_type_code_2,
-    vehicle_type_code_3,
-    vehicle_type_code_4,
-    vehicle_type_code_5
-
 FROM 
     formatted_data
 
