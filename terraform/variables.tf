e07065e2cf751c8d9c04e2d25cabaf128587e732
inner-outer-space <lindasteinberger@gmail.com>
2024-05-04 15:16:06 +0200

    add notes to variable file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,44 +2,40 @@ variable "credentials"  {
   description = "Credentials for GCP"
   default     = "../mage/google_cloud_key.json"
 }  
-
-variable "gcp_storage_location" {
-  description = "Location for GCP bucket"
-  default     = "EU"
-}
-
 variable project {
   description = "Project"
-  default     = "collisions-pipeline"
+  default     = "collisions-pipeline"     # This must be changed to your GCP project
+}
+variable "gcs_bucket_name" {
+  description = "Name for GCP bucket"
+  default     = "nyc-collisions-bucket"   # This must be changed to your globally unique bucket name
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery dataset"
+  default     = "nyc_collisions_dataset"  
+}
+variable "gcp_storage_location" {
+  description = "Location for GCP bucket"
+  default     = "EU"                      # change to your GCP location
 }
 
 variable "region" {
   description = "Region"
-  default     = "europe-west1"
+  default     = "europe-west1"            # change to your GCP region
   
 }
 
 variable "zone" {
   type        = string
   description = "The default compute zone"
-  default     = "europe-west1-b"
-}
-
-variable "bq_dataset_name" {
-  description = "My BigQuery dataset"
-  default     = "nyc_collisions_dataset"
+  default     = "europe-west1-b"          # change to your GCP zone
 }
 
 variable "gcp_storage_class" {
   description = "Storage class for GCP bucket"
   default     = "STANDARD"
 }
-
-variable "gcs_bucket_name" {
-  description = "Name for GCP bucket"
-  default     = "nyc-collisions-bucket"
-}
-
 
 #variable "vm_instance_name" {
 #  description = "Name for GCP VM instance"
