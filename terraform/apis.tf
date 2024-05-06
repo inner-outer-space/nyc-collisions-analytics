0f0b69d62d670df621ad7a95c2e733987f2ef8fd
inner-outer-space <lindasteinberger@gmail.com>
2024-05-06 09:06:39 +0200

    disable apis on destroy

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -3,30 +3,30 @@
 # #############################################
 
 # Enable IAM API
-resource "google_project_service" "iam" {
+resource "google_project_apis" "iam" {
   service = "iam.googleapis.com"
   disable_on_destroy = false
 }
 
 # Enable Cloud Resource Manager API
-resource "google_project_service" "resourcemanager" {
+resource "google_project_apis" "resourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = false
 }
 
-resource "google_project_service" "serviceusage" {
+resource "google_project_apis" "serviceusage" {
   project = var.project
   service = "serviceusage.googleapis.com"
   disable_on_destroy = false
 }
 
-resource "google_project_service" "bigquery" {
+resource "google_project_apis" "bigquery" {
   service = "bigquery.googleapis.com"
   disable_on_destroy = false
 }
 
 # # Enable Cloud SQL Admin API
-# resource "google_project_service" "sqladmin" {
+# resource "google_project_apis" "sqladmin" {
 #   service = "sqladmin.googleapis.com"
 #   disable_on_destroy = false
 # }
