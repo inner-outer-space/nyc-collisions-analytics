801d74f3752cf2de6cc1b0f688c849cd386dfa2d
inner-outer-space <lindasteinberger@gmail.com>
2024-05-06 11:44:25 +0200

    terraform add cli api enable and wait

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,21 +1,38 @@
 # #############################################
 # #               Enable API's                #
 # #############################################
+# Resource: https://stackoverflow.com/questions/59055395/can-i-automatically-enable-apis-when-using-gcp-cloud-with-terraform
+#
 # Enable the following 2 APIs via the console: 
-#   - gcloud services enable serviceusage.googleapis.com
-#   - gcloud services enable bigquery.googleapis.com
+#     gcloud services enable serviceusage.googleapis.com bigquery.googleapis.com
 
+# Use `gcloud` to enable:
+# - serviceusage.googleapis.com
+# - bigquery.googleapis.com
+resource "null_resource" "enable_service_usage_and_bq_api" {
+  provisioner "local-exec" {
+    command = "gcloud services enable serviceusage.googleapis.com bigquery.googleapis.com --project ${var.project}"
+  }
+}
+
+# Wait for the new configuration to propagate
+resource "time_sleep" "wait_api_init" {
+  create_duration = "60s"
+  depends_on = [null_resource.enable_service_usage_and_bq_api]
+}
 
 # Enable IAM API
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
   disable_on_destroy = false
+  depends_on = [time_sleep.wait_api_init]
 }
 
 # Enable Cloud Resource Manager API
 resource "google_project_service" "resourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = false
+  depends_on = [time_sleep.wait_api_init]
 }
 
 # This needs to be enabled manually in the GCP console
--- terraform/services.tf
+++ terraform/services.tf
@@ -6,9 +6,11 @@ resource "google_storage_bucket" "collisions-bucket" {
   name = var.gcs_bucket_name
   location = var.gcp_storage_location
   force_destroy = true
+  depends_on = [time_sleep.wait_api_init]
 }
 
 resource "google_bigquery_dataset" "collisions-dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.gcp_storage_location
+  depends_on = [time_sleep.wait_api_init]
 }
